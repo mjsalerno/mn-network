6cb62654779ae87e2bd33373cc81675a0521734a
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-11-13 22:06:18 -0500

    fixed the script

--- create-network.py
+++ create-network.py
@@ -16,9 +16,9 @@ class MyTopo(Topo):
 
 
         #DMZ
-        dmz_sw = self.addSwitch('dmz_sw')
-        dmz_srv = self.addHost('dmz_srv')
-        dmz_fw = self.addHost('dmz_fw')
+        dmz_sw = self.addSwitch('s1')
+        dmz_srv = self.addHost('h1')
+        dmz_fw = self.addHost('h2')
 
         self.addLink(dmz_sw, dmz_srv)
         self.addLink(dmz_sw, dmz_fw)
@@ -26,9 +26,9 @@ class MyTopo(Topo):
 
 
         #Apps
-        apps_sw = self.addSwitch('apps_sw')
-        apps_srv = self.addHost('apps_srv')
-        apps_fw = self.addHost('apps_fw')
+        apps_sw = self.addSwitch('s2')
+        apps_srv = self.addHost('h3')
+        apps_fw = self.addHost('h4')
 
         self.addLink(apps_sw, apps_srv)
         self.addLink(apps_sw, apps_fw)
@@ -36,16 +36,16 @@ class MyTopo(Topo):
 
 
         #Dept 1
-        d1_sw1 = self.addSwitch('d1_sw1')
-        d1_sw2 = self.addSwitch('d1_sw2')
-        d1_sw3 = self.addSwitch('d1_sw3')
-        d1_wifi = self.addSwitch('d1_wifi')
-        d1_host1 = self.addHost('d1_host1')
-        d1_host2 = self.addHost('d1_host2')
-        d1_host3 = self.addHost('d1_host3')
-        d1_host4 = self.addHost('d1_host4')
-        d1_host5 = self.addHost('d1_host5')
-        d1_host6 = self.addHost('d1_host6')
+        d1_sw1 = self.addSwitch('s3')
+        d1_sw2 = self.addSwitch('s4')
+        d1_sw3 = self.addSwitch('s5')
+        d1_wifi = self.addSwitch('s6')
+        d1_host1 = self.addHost('h5')
+        d1_host2 = self.addHost('h6')
+        d1_host3 = self.addHost('h8')
+        d1_host4 = self.addHost('h9')
+        d1_host5 = self.addHost('h10')
+        d1_host6 = self.addHost('h11')
 
         self.addLink(d1_sw1, d1_sw2)
         self.addLink(d1_sw1, d1_sw3)
@@ -59,17 +59,16 @@ class MyTopo(Topo):
 
 
         #Dept 2
-        d2_sw1 = self.addSwitch('d2_sw1')
-        d2_sw2 = self.addSwitch('d2_sw2')
-        d2_sw3 = self.addSwitch('d2_sw3')
-        d2_wifi = self.addSwitch('d2_wifi')
-        d2_host1 = self.addHost('d2_host1')
-        d2_host2 = self.addHost('d2_host2')
-        d2_host3 = self.addHost('d2_host3')
-        d2_host4 = self.addHost('d2_host4')
-        d2_host5 = self.addHost('d2_host5')
-        d2_host6 = self.addHost('d2_host6')
-
+        d2_sw1 = self.addSwitch('s7')
+        d2_sw2 = self.addSwitch('s8')
+        d2_sw3 = self.addSwitch('s9')
+        d2_wifi = self.addSwitch('s10')
+        d2_host1 = self.addHost('h12')
+        d2_host2 = self.addHost('h13')
+        d2_host3 = self.addHost('h14')
+        d2_host4 = self.addHost('h15')
+        d2_host5 = self.addHost('h16')
+        d2_host6 = self.addHost('h17')
         self.addLink(d2_sw1, d2_sw2)
         self.addLink(d2_sw1, d2_sw3)
         self.addLink(d2_sw1, d2_wifi)
@@ -82,7 +81,7 @@ class MyTopo(Topo):
 
 
         #Core
-        core = self.addSwitch('core')
+        core = self.addSwitch('s100')
         self.addLink(core, dmz_sw)
         self.addLink(core, apps_sw)
         self.addLink(core, d1_sw1)
