a4e071edb676788c6ed8cd9495f0be7e77eb921c
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-11-13 21:52:27 -0500

    first attempt at modeling the diagram

--- create-network.py
+++ create-network.py
@@ -13,16 +13,80 @@ class MyTopo(Topo):
         # Initialize topology
         Topo.__init__(self)
 
-        # Add hosts and switches
-        leftHost = self.addHost('h1')
-        rightHost = self.addHost('h2')
-        leftSwitch = self.addSwitch('s3')
-        rightSwitch = self.addSwitch('s4')
 
-        # Add links
-        self.addLink(leftHost, leftSwitch)
-        self.addLink(leftSwitch, rightSwitch)
-        self.addLink(rightSwitch, rightHost)
+
+        #DMZ
+        dmz_sw = self.addSwitch('dmz_sw')
+        dmz_srv = self.addHost('dmz_srv')
+        dmz_fw = self.addHost('dmz_fw')
+
+        self.addLink(dmz_sw, dmz_srv)
+        self.addLink(dmz_sw, dmz_fw)
+
+
+
+        #Apps
+        apps_sw = self.addSwitch('apps_sw')
+        apps_srv = self.addHost('apps_srv')
+        apps_fw = self.addHost('apps_fw')
+
+        self.addLink(apps_sw, apps_srv)
+        self.addLink(apps_sw, apps_fw)
+
+
+
+        #Dept 1
+        d1_sw1 = self.addSwitch('d1_sw1')
+        d1_sw2 = self.addSwitch('d1_sw2')
+        d1_sw3 = self.addSwitch('d1_sw3')
+        d1_wifi = self.addSwitch('d1_wifi')
+        d1_host1 = self.addHost('d1_host1')
+        d1_host2 = self.addHost('d1_host2')
+        d1_host3 = self.addHost('d1_host3')
+        d1_host4 = self.addHost('d1_host4')
+        d1_host5 = self.addHost('d1_host5')
+        d1_host6 = self.addHost('d1_host6')
+
+        self.addLink(d1_sw1, d1_sw2)
+        self.addLink(d1_sw1, d1_sw3)
+        self.addLink(d1_sw1, d1_wifi)
+        self.addLink(d1_sw2, d1_host1)
+        self.addLink(d1_sw2, d1_host2)
+        self.addLink(d1_sw3, d1_host3)
+        self.addLink(d1_sw3, d1_host4)
+        self.addLink(d1_wifi, d1_host5)
+        self.addLink(d1_wifi, d1_host6)
+
+
+        #Dept 2
+        d2_sw1 = self.addSwitch('d2_sw1')
+        d2_sw2 = self.addSwitch('d2_sw2')
+        d2_sw3 = self.addSwitch('d2_sw3')
+        d2_wifi = self.addSwitch('d2_wifi')
+        d2_host1 = self.addHost('d2_host1')
+        d2_host2 = self.addHost('d2_host2')
+        d2_host3 = self.addHost('d2_host3')
+        d2_host4 = self.addHost('d2_host4')
+        d2_host5 = self.addHost('d2_host5')
+        d2_host6 = self.addHost('d2_host6')
+
+        self.addLink(d2_sw1, d2_sw2)
+        self.addLink(d2_sw1, d2_sw3)
+        self.addLink(d2_sw1, d2_wifi)
+        self.addLink(d2_sw2, d2_host1)
+        self.addLink(d2_sw2, d2_host2)
+        self.addLink(d2_sw3, d2_host3)
+        self.addLink(d2_sw3, d2_host4)
+        self.addLink(d2_wifi, d2_host5)
+        self.addLink(d2_wifi, d2_host6)
+
+
+        #Core
+        core = self.addSwitch('core')
+        self.addLink(core, dmz_sw)
+        self.addLink(core, apps_sw)
+        self.addLink(core, d1_sw1)
+        self.addLink(core, d2_sw1)
 
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
